0e096e7077f853ad746650a57ca8efa1703d267b
Expellishock81637 <due93196@gmail.com>
2024-11-09 22:39:45 +0800

    Initial Commit

---
+++ Ebook APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ebook APP
+//
+//  Created by Williard Liao on 2024/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ebook APP/Ebook_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ebook_APPApp.swift
+//  Ebook APP
+//
+//  Created by Williard Liao on 2024/11/9.
+//
+
+import SwiftUI
+
+@main
+struct Ebook_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
